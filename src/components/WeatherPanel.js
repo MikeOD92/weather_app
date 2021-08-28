488c1279c50a17ecc07df04a104415b36e47acaf
Michael <michaelodell660@gmail.com>
2021-08-28 15:02:33 -0700

    working on desktop view changes font sizes and layout of details panel

--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -27,15 +27,15 @@ export default function WeatherPanel(props){
                     <h2 className="loc"> {props.weatherData? `${props.weatherData.name}, ${props.weatherData.sys.country}` : ''} </h2>
                     <p className="cords">{props.weatherData? `Lat: ${props.weatherData.coord.lat}, Lon: ${props.weatherData.coord.lon}`:''}</p>
                     <h3 className="temp"> {props.weatherData? `${props.weatherData.main.temp}` : ''}<WiFahrenheit/></h3>
-                    <p> {props.weatherData? `feels like: ${props.weatherData.main.feels_like} `: ''}<WiFahrenheit/></p>
+                    <p className="realFeel"> {props.weatherData? `feels like: ${props.weatherData.main.feels_like} `: ''}<WiFahrenheit/></p>
                     <p className="condition"> {props.weatherData? `${props.weatherData.weather[0].description}` : ''}</p>
                 </div>
                 
             </div>
             <div className="details">
-                <p> {props.weatherData? `high: ${props.weatherData.main.temp_max} | low: ${props.weatherData.main.temp_min}`: ''}</p>
-                <p> {props.weatherData? `humidity: ${props.weatherData.main.humidity}%`: ''}</p>
-                <p> {props.weatherData? `wind: ${props.weatherData.wind.deg} deg  at ${props.weatherData.wind.speed}mph`: ''}</p>
+                <p className="highLow"> {props.weatherData? `high: ${props.weatherData.main.temp_max} | low: ${props.weatherData.main.temp_min}`: ''}</p>
+                <p className="humidity"> {props.weatherData? `humidity: ${props.weatherData.main.humidity}%`: ''}</p>
+                <p className="wind"> {props.weatherData? `wind: ${props.weatherData.wind.deg} deg  at ${props.weatherData.wind.speed}mph`: ''}</p>
                 {props.saved.includes(props.weatherData.name)? <button className='btn btn-info' onClick={deleteSaved}>Delete</button>: ''}
             </div>
         </div>
